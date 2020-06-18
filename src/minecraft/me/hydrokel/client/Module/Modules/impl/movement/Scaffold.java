84c8056f57fb3e465eadd98c36d1cd21be9e27b7
hytykateur <lebgbn@outlook.fr>
2020-06-18 18:38:37 +0200

    Fixed some bugs in scaffold module

--- src/minecraft/me/hydrokel/client/Module/Modules/impl/movement/Scaffold.java
+++ src/minecraft/me/hydrokel/client/Module/Modules/impl/movement/Scaffold.java
@@ -22,36 +22,42 @@ public class Scaffold extends Module {
     public Scaffold() {
         super("Scaffold",0, Category.MOVEMENT);
     }
+
     @EventTarget
     public void onMotion(EventMotion e) {
         AxisAlignedBB box = this.mc.thePlayer.getEntityBoundingBox();
         AxisAlignedBB ajusted_box = box.offset(0, -0.5, 0).expand(-0.001, 0, -0.001);
          Stream<AxisAlignedBB> block_collisions = this.mc.theWorld.getCollidingBoundingBoxes(this.mc.thePlayer, ajusted_box).stream();
-        if (block_collisions.findAny().isPresent()) {
-            return;
-        }
+                if (block_collisions.findAny().isPresent()) {
+                    return;
+                }
 
-            BlockPos p_block = this.getProximityBlock(this.mc.thePlayer.);
-        if (p_block == null) {
-            return;
-        }
+            BlockPos p_block = this.getProximityBlock(this.mc.thePlayer);
+            if (p_block == null) {
+                return;
+            }
             Vec3 block_vector = new Vec3(p_block);
             RotationsUtils.Rotation needed_rotations = RotationsUtils.getNeededRotations(block_vector);
             e.setYaw(needed_rotations.getYaw());
-            e.setPitch(100);
             //OOF
-            this.mc.playerController.onPlayerRightClick(this.mc.thePlayer,this.mc.theWorld,this.mc.thePlayer.getHeldItem(),p_block, this.mc.thePlayer.getHorizontalFacing(),block_vector);
+            try {
+
+                this.mc.playerController.onPlayerRightClick(this.mc.thePlayer,this.mc.theWorld,this.mc.thePlayer.getHeldItem(),p_block, this.mc.thePlayer.getHorizontalFacing(),block_vector);
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            }
 
     }
     public BlockPos getProximityBlock(EntityPlayer e) {
         Random r = new Random();
+
         double p_x = e.posX;
-        double p_z = e.posY;
+        double p_z = e.posZ;
         BlockPos block_out;
         List<BlockPos> block_arround = new ArrayList<BlockPos>();
-        for (int x = 0;x < 3;x++) {
-            for (int y = 0;y < 3;y++) {
-                BlockPos temp_block = new BlockPos(p_x - 1 + x , e.posY - 1,p_z - 1 + y);
+        for (int x = 0;x < 2;x++) {
+            for (int y = 0;y < 2;y++) {
+                BlockPos temp_block = new BlockPos(p_x - 1 + x , e.getPosition().getY() -1,p_z - 1 + y);
                 if (this.mc.theWorld.getBlockState(temp_block).getBlock().getMaterial() != Material.air) {
                     block_arround.add(temp_block);
                 }
